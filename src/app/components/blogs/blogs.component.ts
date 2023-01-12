794dd2b6c58227c4169e780b55a87cd91ec7697e
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2023-01-12 17:45:54 +0530

    blogs added

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.scss']
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit{
+  tempArr : any = [
+    {
+      id:1,
+      text:"One"
+    },
+    {
+      id:2,
+      text:"Two"
+    },
+    {
+      id:3,
+      text:"Two"
+    },
+    {
+      id:4,
+      text:"Four"
+    },
+    {
+      id:5,
+      text:"Five"
+    },
+    {
+      id:6,
+      text:"Six"
+    },
+    {
+      id:7,
+      text:"Seven"
+    }
+  ];
+  ngOnInit(): void {
+  }
 
 }
