6901ac7f3f07eed60c26fb092aca90b7c5c68c20
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2023-01-18 11:57:47 +0530

    Added skills section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
 { path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
 { path: 'header', loadChildren: () => import('./components/header/header.module').then(m => m.HeaderModule) }, 
 { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
-{ path: 'blogs', loadChildren: () => import('./components/blogs/blogs.module').then(m => m.BlogsModule) }
+{ path: 'blogs', loadChildren: () => import('./components/blogs/blogs.module').then(m => m.BlogsModule) },
+{ path: 'skills', loadChildren: () => import('./components/skills/skills.module').then(m => m.SkillsModule) }
 ];
 
 @NgModule({
